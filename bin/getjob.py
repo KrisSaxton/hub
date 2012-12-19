d976efa0b760c232438e34505d7e7655ab0e3ff0
Matthew Mead-Briggs <matthew@automationlogic.com>
2012-12-19 12:15:36 +0000

    externalised the hub client for creating, updating and getting jobs.

--- bin/getjob.py
+++ bin/getjob.py
@@ -1,58 +1,13 @@
 #!/usr/bin/env python
 import sys
-import pika
-import json
-import uuid
 import hub.lib.config as config
 
 config_file = '/usr/local/pkg/hub/etc/hub.conf'
 
-try:
-    conf = config.setup(config_file)
-except error.ConfigError, e:
-    print e.msg
-    raise e
-
-class Client(object):
-
-    '''Class representing things that can get jobs.'''
-
-    def __init__(self):
-        self.conf = config.setup()
-        self.broker = conf.get('HUB', 'broker')
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host=self.broker))
-        self.channel = self.connection.channel()
-        result = self.channel.queue_declare(exclusive=True)
-        self.callback_queue = result.method.queue
-        self.channel.basic_consume(
-                        self.on_response,
-                        no_ack=True,
-                        queue=self.callback_queue)
-
-    def on_response(self, channel, method, properties, body):
-        if self.corr_id == properties.correlation_id:
-            self.response = body    
-
-    def post(self, jobid):
-        '''Takes a job as python dict and posts as json object.'''
-        self.response = None
-        self.corr_id = str(uuid.uuid4())
-        print 'Submitting status request for job with id %s' % jobid
-        self.channel.basic_publish(exchange='',
-                         routing_key='hub_status',
-                         properties=pika.BasicProperties(
-                              content_type='application/json',
-                              reply_to = self.callback_queue,
-                              correlation_id = self.corr_id,
-                              ),
-                         body=json.dumps(jobid))
-        while self.response is None:
-            self.connection.process_data_events()
-        return str(self.response)
+from hub.lib.client import Client
 
 if __name__ == '__main__':
     jobid = sys.argv[1]
-    client = Client()
-    response = client.post(jobid)
+    client = Client(config_file)
+    response = client.post_wait(jobid, 'get')
     print response
--- bin/putresult.py
+++ bin/putresult.py
@@ -1,46 +1,14 @@
 #!/usr/bin/env python
 import sys
-import pika
-import json
-import uuid
 import hub.lib.config as config
 
 config_file = '/usr/local/pkg/hub/etc/hub.conf'
 
-try:
-    conf = config.setup(config_file)
-except error.ConfigError, e:
-    print e.msg
-    raise e
-
-class Client(object):
-
-    '''Class representing things that can submit jobs.'''
-
-    def __init__(self):
-        self.conf = config.setup()
-        self.broker = conf.get('HUB', 'broker')
-        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
-            host=self.broker))
-        self.channel = self.connection.channel()
-
-    def post(self, job_id, taskdata):
-        '''Takes task data as a json ojbect and posts to dispatcher.'''
-        self.response = None
-        self.channel.basic_publish(exchange='',
-                         routing_key='hub_results',
-                         properties=pika.BasicProperties(
-                              correlation_id = str(job_id),
-                              content_type='application/json',
-                              ),
-                         body=json.dumps(taskdata))
-        while self.response is None:
-            self.connection.process_data_events()
-        return str(self.response)
+from hub.lib.client import Client
 
 if __name__ == '__main__':
     job_id = sys.argv[1]
     taskdata = {'status': 'SUCCESS', 'name': 'add', 'args': [1, 2], 'parent_id': job_id, 'data': 4, 'id': '277e2421-e7a0-11e1-af41-109adda719a8'}
-    client = Client()
-    client.post(job_id, taskdata)
+    client = Client(config_file)
+    client.post(job_id, 'update', taskdata=taskdata)
     print 'Successfully submitted task results %s for job with id: %s' % (taskdata, job_id)
